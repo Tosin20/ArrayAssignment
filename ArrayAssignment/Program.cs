6824c8b6f21eec30672e91b18d093abff3989324
Support <Support@DESKTOP-7DS253L>
2022-08-05 11:14:35 +0100

    Add project files.

---
+++ ArrayAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SortArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newNumbers = new int[] { 1, 7, 5, 2, 10, 4, 8, 3, 6, 9 };
+            int num;
+
+            for (int i = 0; i < newNumbers.Length; i++)
+            {
+                for (int sort = i + 1; sort < newNumbers.Length; sort++)
+                {
+                    if (newNumbers[i] < newNumbers[sort])
+                    {
+                        num = newNumbers[i];
+                        newNumbers[i] = newNumbers[sort];
+                        newNumbers[sort] = num;
+                    }
+                }
+            }
+            foreach (int values in newNumbers)
+            {
+                Console.WriteLine(values + " ");
+            }
+
+        }
+    }
+}
+
+
+
+
